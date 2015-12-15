7822e982e5c525e26fbdd4cbde7d876da5aa5a66
Yamil <yamil.nunez@gmail.com>
2015-12-15 08:37:42 -0500

    remove folder

--- Desktop/operativos/GUI/chat/sockets/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-import sockets.views
-
-urlpatterns = patterns('encuestas.formularios.urls',
-	url(r'^$', sockets.views.Index.as_view(), name="index"),
-	url(r'^conectar/$', sockets.views.Conectar, name="index"),
-	url(r'^desconectar/$', sockets.views.Desconectar, name="index"),
-)
